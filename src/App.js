e866e07e252da61b9592a00c645454141e7de5f8
Nicolò Cappelletto <nicolo.cappelletto@ennova-research.com>
2026-01-03 15:26:40 +0100

    Replace app with verify screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import NavBar from './components/NavBar'
-import ExercisesList from './components/ExercisesList'
 
 const App = () => (
-  <>
-    <h1>Pipeline Verification: Success!</h1>
-    <NavBar />
-    <ExercisesList />
-  </>
+  <div style={{ backgroundColor: 'yellow', height: '100vh', padding: '50px' }}>
+    <h1 style={{ color: 'red', fontSize: '50px' }}>
+      PIPELINE SUCCESS!
+    </h1>
+    <h2>
+      Automation works.
+    </h2>
+  </div>
 )
 
 export default App
